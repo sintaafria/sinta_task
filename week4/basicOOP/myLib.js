9c9efe21c8e5c96362fe7c97ea0c7cb730efbc5f
sintaafria <sintaafria16@gmail.com>
2021-10-15 14:33:37 +0700

    add new library and src directory

--- week4/basicOOP/myLib.js
+++ week4/basicOOP/myLib.js
@@ -28,3 +28,28 @@ class Table {
 		element.innerHTML = thead + tbody;
 	}
 }
+
+class Card {
+	constructor(content) {
+		this.content = content;
+	}
+
+	render(element) {
+		let item = "";
+		for(let i=0; i<this.content.length; i++) {
+			let col = this.content[i];
+			item += `
+					<div class="col my-3">
+		          		<div class="card">
+		            		<img src=${col[0]} class="card-img-top" alt="...">
+		            		<div class="card-body">
+		              			<p class="card-text">${col[1]}</p>
+		            		</div>
+		          		</div>
+		        	</div>`;
+		}
+
+		element.innerHTML = `<div class="row row-cols-2 row-cols-sm-2 row-cols-md-3 row-cols-lg-4">
+								${item} </div>`
+	}
+}
